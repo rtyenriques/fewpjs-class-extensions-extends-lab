2627d3fce7b8a5ba4f20c03cc2664d9a5e767705
= <rtyenriques@gmail.com>
2022-01-12 20:49:35 -0800

    commit tree 8eead40bb1eb43cc05a7e3502f071394e1127d18

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ class Polygon {
 
 class Triangle extends Polygon{
     get isValid() {
-    //    let a = this.sides[0]
-    //    let b = this.sides[1]
-    //    let c = this.sides[2]
-    let a,b,c = [this.sides[0], this.sides[1],this.sides[2]]
+       let a = this.sides[0]
+       let b = this.sides[1]
+       let c = this.sides[2]
+
        const con1 = a + b > c;
        const con2 = b + c > a;
        const con3 = c + a > b;
